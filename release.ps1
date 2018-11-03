af827f8dbff573ace50e36e90ad0e5648a679830
Mark Makarychev <markmak77@gmail.com>
2018-11-03 15:35:27 +0300

    Updated the release script.

--- release.ps1
+++ release.ps1
@@ -10,10 +10,15 @@ if ($component.version -ne $version) {
     throw "Versions in component.json and pom.xml do not match"
 }
 
-# TODO: switch npm to maven
-# Automatically login to server
-# if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null) {
-#     npm-cli-login
-# }
-
-# npm publish
+# Make sure that:
+# 1. GPG has been installed, key was generated and uploaded to a key server;
+# 2. Maven has been installed (use v3.3.9 instead of v3.5.4 - otherwise upload will be VERY slow);
+# 3. Maven's global settings file (~/.m2/settings.xml) contains all necessary credentials and your key
+# 4. ~/.gnupg contains 2 files, each of which contain the lines in double quotation marks:
+#   4a. ~/.gnupg/gpg-agent.conf
+#   "allow-loopback-pinentry"
+#   4b. ~/.gnupg/gpg.conf
+#   "pinentry-mode loopback"
+# 
+# See "Development.md" in the "doc" folder for detailed instructions.
+mvn clean deploy
